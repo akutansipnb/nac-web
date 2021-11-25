e21b3e4772cbad27d7ea7ef08b0e04e3eecc27f2
suryajayantara <ptsuryajayantara01@gmail.com>
2021-11-25 16:36:55 +0800

    :smile: Adding db-raw text documentation

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    {{-- Tailwinds --}}
+
+    <link rel="stylesheet" href={{asset('css/app.css')}}>
+    
+    {{-- Custom CSS --}}
+    
+    <link rel="stylesheet" href={{asset('css/style.css')}}>
+
+    {{-- Other --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    
+
+</head>
+<body>
+    
+
+        <section class="hero-section">
+            <div class="banner-logo"> <h1>National</h1> <h1 class="gradient-text">Accounting</h1> <h1>Competition</h1></div>
+            <div class="px-10 md:px-52 text-center my-10"> <p class="championship-theme">"Melalui Kompetisi Wujudkan Sumber Daya Manusia di bidang Akuntansi dan Perpajakan yang Berkualitas, Kompetitif dan Berintegritas di Era Digital"</p> </div>
+            <div class="flex-col">
+                <button class="btn-orange py-2 px-4 md:py-4 md:px-12 mx-4">Daftar Sekarang</button>
+                <button class="btn-secondary py-2 px-4 md:py-4 md:px-12 mx-4">Pelajari Lebih Lanjut</button>
+            </div>
+        </section>
+
+</body>
+</html>
---
+++ resources/views/tailtest.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    {{-- Tailwinds --}}
+
+    <link rel="stylesheet" href={{asset('css/app.css')}}>
+    
+    {{-- Custom CSS --}}
+    
+    <link rel="stylesheet" href={{asset('css/style.css')}}>
+    
+
+</head>
+<body>
+    
+    <div class="container my-5">
+        {{-- No Tailwinds --}}
+        <p class="text-3xl font-light"> SCSS Without <span class="font-bold">Tailwind</span> class </p>
+        <hr class="my-5">
+        <button class="btn-orange">Orange Button</button>
+        <button class="btn-secondary">White Button</button>
+    </div>
+
+    <div class="container my-5">
+        {{-- With Tailwinds --}}
+        <p class="text-3xl font-light"> SCSS With <span class="font-bold">Tailwind</span> class </p>
+        <hr class="my-5">
+        <button class="btn-orange py-4 px-12">Orange Button</button>
+        <button class="btn-secondary py-4 px-12">White Button</button>
+    </div>
+
+</body>
+</html>
