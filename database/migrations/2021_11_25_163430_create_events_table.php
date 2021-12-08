204b22b095a4bf02c87fa6660a3a592ebda08bb6
suryajayantara <ptsuryajayantara01@gmail.com>
2021-12-08 12:24:18 +0800

    :sparkles: argon modifier

--- database/migrations/2021_11_25_163430_create_events_table.php
+++ database/migrations/2021_11_25_163430_create_events_table.php
@@ -21,6 +21,7 @@ class CreateEventsTable extends Migration
             $table->enum('audience',['university','school']);
             $table->date('tm_time')->nullable(true);
             $table->enum('tm_method',['youtube','zoom'])->nullable(true);
+            $table->integer('registration fee')->nullable('false');
             $table->integer('max_slot')->nullable(false);
             $table->date('register_time');
             $table->date('register_close');
