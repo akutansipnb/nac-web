40e7cec484f28866322a8864347d2e24b02003b8
adearta <bagusmadearta01@gmail.com>
2021-12-11 19:28:37 +0800

    :sparkles: making some features register

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\EventsController;
 use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Builder\Class_;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,7 @@ use PhpParser\Builder\Class_;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-// 
+//
 */
 
 Route::get('/', [LandingController::class,'index']);
@@ -22,6 +23,17 @@ Route::get('/blog', function () {
     return view('register.university');
 });
 
+Route::get('/school', function () {
+    return view('register.school');
+});
+
+Route::get('/regschool', function () {
+    return view('register.academy.school');
+});
+
+Route::get('/reguniversity', function () {
+    return view('register.academy.university');
+});
 
 Route::get('/dashboard', function () {
     return view('admin.index');
@@ -37,7 +49,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 route::prefix('dashboard')->group(function(){
 
-    
+
     Route::get('/', function () {
         return view('admin.index');
     });
