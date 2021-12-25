55c7eca3a8249e564dc527bf4cddcd33ff0aa305
adearta <bagusmadearta01@gmail.com>
2021-12-25 15:45:46 +0800

    :sparkles: adding feature CRUD time schedule

---
+++ app/Models/TimeSchedule.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TimeSchedule extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'schedule',
+        'date_start',
+        'date_end',
+        'desc'
+    ];
+}
