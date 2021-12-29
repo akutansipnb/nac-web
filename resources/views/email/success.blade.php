917a336b579cbc6c19bbae947f0d973f246daec8
suryajayantara <ptsuryajayantara01@gmail.com>
2021-12-29 19:01:40 +0800

    :bug: git push

--- resources/views/email/success.blade.php
+++ resources/views/email/success.blade.php
@@ -1,12 +1,16 @@
 @component('mail::message')
-# Introduction
 
-The body of your message.
+# Halo , <strong>{{$name}}</strong>
+
+
+Terima Kasih sudah bergabung menjadi peserta NAC 2021 cabang lomba <strong> {{$event}} {{$year}}</strong>  , Segera selesaikan pembayaranmu yaa
 
 @component('mail::button', ['url' => ''])
-Button Text
+Masuk ke Dashboard
 @endcomponent
 
-Thanks,<br>
+Salam Hangat,<br>
+<br>
+Panitia National Accouting Competition 
 {{ config('app.name') }}
 @endcomponent
