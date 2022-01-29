d15e68cedffae4431ebb5d81c6b14a9a057ad7a9
suryajayantara <ptsuryajayantara01@gmail.com>
2022-01-29 21:40:49 +0800

    :bug: fixing validation controller

--- app/Http/Controllers/User/ValidationController.php
+++ app/Http/Controllers/User/ValidationController.php
@@ -26,64 +26,64 @@ class ValidationController extends Controller
     public function updateValidation(Request $request){
         $user = Auth::user()->details;
 
-        // $event = Auth::user()->details->events;
-        // $event_name = $event->aliases.$event->year;
+        $event = Auth::user()->details->events;
+        $event_name = $event->aliases.$event->year;
 
-        // $url = 'img/participant/'.$event_name.'/'. strtolower(str_replace(' ','_',Auth::user()->name));
+        $url = 'img/participant/'.$event_name.'/'. strtolower(str_replace(' ','_',Auth::user()->name));
 
-        // $validation_1 = $request->file('validation_1');
-        // $validation_1_name =  $user->identity_code . "-" . str_replace(' ','_',Auth::user()->name) . "-" . $user->events->aliases . $user->events->year. "." . $validation_1->getClientOriginalExtension();
+        $validation_1 = $request->file('validation_1');
+        $validation_1_name =  $user->identity_code . "-" . str_replace(' ','_',Auth::user()->name) . "-" . $user->events->aliases . $user->events->year. "." . $validation_1->getClientOriginalExtension();
 
 
-        // $validation_2 = $request->file('validation_2');
-        // $validation_2_name = $user->identity_code . "-" . str_replace(' ','_',Auth::user()->name) . "-" . $user->events->aliases . $user->events->year. "." . $validation_2->getClientOriginalExtension();
+        $validation_2 = $request->file('validation_2');
+        $validation_2_name = $user->identity_code . "-" . str_replace(' ','_',Auth::user()->name) . "-" . $user->events->aliases . $user->events->year. "." . $validation_2->getClientOriginalExtension();
 
-        // $validation_3 = $request->file('validation_3');
-        // $validation_3_name = $user->identity_code . "-" . str_replace(' ','_',Auth::user()->name) . "-" . $user->events->aliases . $user->events->year. "." . $validation_3->getClientOriginalExtension();
+        $validation_3 = $request->file('validation_3');
+        $validation_3_name = $user->identity_code . "-" . str_replace(' ','_',Auth::user()->name) . "-" . $user->events->aliases . $user->events->year. "." . $validation_3->getClientOriginalExtension();
 
-        // $update = [
-        //     'validation_1' => $url.'/ktm-'.$validation_1_name,
-        //     'validation_2' => $url.'/pernyataan-'.$validation_2_name,
-        //     'validation_3' => $url.'/kuitansi-'.$validation_3_name,
-        //     'validation_status' => 'pending'
-        // ];
+        $update = [
+            'validation_1' => $url.'/ktm-'.$validation_1_name,
+            'validation_2' => $url.'/pernyataan-'.$validation_2_name,
+            'validation_3' => $url.'/kuitansi-'.$validation_3_name,
+            'validation_status' => 'pending'
+        ];
 
-        // if(Auth::user()->details->events->audience === 'school'){
-        //     $validation_4 = $request->file('validation_4');
-        //     $validation_4_name = $user->identity_code . "-" . str_replace(' ','_',Auth::user()->name) . "-" . $user->events->aliases . $user->events->year. "." . $validation_4->getClientOriginalExtension();
+        if(Auth::user()->details->events->audience === 'school'){
+            $validation_4 = $request->file('validation_4');
+            $validation_4_name = $user->identity_code . "-" . str_replace(' ','_',Auth::user()->name) . "-" . $user->events->aliases . $user->events->year. "." . $validation_4->getClientOriginalExtension();
 
-        //     $validation_5 = $request->file('validation_5');
-        //     $validation_5_name = $user->identity_code . "-" . str_replace(' ','_',Auth::user()->name) . "-" . $user->events->aliases . $user->events->year. "." . $validation_5->getClientOriginalExtension();
+            $validation_5 = $request->file('validation_5');
+            $validation_5_name = $user->identity_code . "-" . str_replace(' ','_',Auth::user()->name) . "-" . $user->events->aliases . $user->events->year. "." . $validation_5->getClientOriginalExtension();
 
-        //     $update = [
-        //         'validation_1' => $url.'/kts-'.$validation_1_name,
-        //         'validation_2' => $url.'/delegasi-'.$validation_2_name,
-        //         'validation_3' => $url.'/kuitansi-'.$validation_3_name,
-        //         'validation_4' => $url.'/foto-'.$validation_4_name,
-        //         'validation_5' => $url.'/integritas-'.$validation_5_name,
-        //         'validation_status' => 'pending'
-        //     ];
-        // }
-        // try {
-        //     UserDetail::where('id',Auth::user()->details->id)->update($update);
-        //     if(Auth::user()->details->events->audience === 'school'){
-        //         $validation_1->move($url,'/kts-'.$validation_1_name);
-        //         $validation_2->move($url,'/delegasi-'.$validation_2_name,);
-        //         $validation_3->move($url,'/kuitansi-'.$validation_3_name);
-        //         $validation_4->move($url,'/foto-'.$validation_4_name);
-        //         $validation_5->move($url,'/integritas-'.$validation_5_name);
-        //     }else{
-        //         $validation_1->move($url,'/ktm-'.$validation_1_name);
-        //         $validation_2->move($url,'/pernyataan-'.$validation_2_name,);
-        //         $validation_3->move($url,'/kuita    nsi-'.$validation_3_name);
-        //     }
-        // } catch (\Throwable $th) {
-        //     throw $th;
+            $update = [
+                'validation_1' => $url.'/kts-'.$validation_1_name,
+                'validation_2' => $url.'/delegasi-'.$validation_2_name,
+                'validation_3' => $url.'/kuitansi-'.$validation_3_name,
+                'validation_4' => $url.'/foto-'.$validation_4_name,
+                'validation_5' => $url.'/integritas-'.$validation_5_name,
+                'validation_status' => 'pending'
+            ];
+        }
+        try {
+            UserDetail::where('id',Auth::user()->details->id)->update($update);
+            if(Auth::user()->details->events->audience === 'school'){
+                $validation_1->move($url,'/kts-'.$validation_1_name);
+                $validation_2->move($url,'/delegasi-'.$validation_2_name,);
+                $validation_3->move($url,'/kuitansi-'.$validation_3_name);
+                $validation_4->move($url,'/foto-'.$validation_4_name);
+                $validation_5->move($url,'/integritas-'.$validation_5_name);
+            }else{
+                $validation_1->move($url,'/ktm-'.$validation_1_name);
+                $validation_2->move($url,'/pernyataan-'.$validation_2_name,);
+                $validation_3->move($url,'/kuita    nsi-'.$validation_3_name);
+            }
+        } catch (\Throwable $th) {
+            throw $th;
         // }
 
         $e = UserDetail::where('identity_code',$user->identity_code)->first(); 
         Mail::to(Auth::user()->email)->send(new VerificationEmail($e));
-        // return redirect()->route('dashboard.index')->with(['success'=>'Berhasil Melakukan Validasi']);
+        return redirect()->route('dashboard.index')->with(['success'=>'Berhasil Melakukan Validasi']);
         // dd($request);
     }
 
