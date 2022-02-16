fd59d6471ad35308c0bd5518ee2546b2c9df01f5
suryajayantara <ptsuryajayantara01@gmail.com>
2022-02-16 15:30:40 +0800

    :bug: fixing somethings

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -135,27 +135,7 @@
                     </span>
                 </div>
 
-                <div class="flex items-center mt-3 sm:ml-6 sm:mt-0">
-                    <span class="flex-shrink-0 p-1 text-white bg-yellow-500 rounded-full">
-                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z">
-                            </path>
-                        </svg>
-                    </span>
-
-                    <span class="
-                  flex
-                  ml-3
-                  text-sm
-                  font-medium
-                  text-gray-600
-                  space-x-1 space-x-reverse
-                ">
-                        <dt>Peserta</dt>
-                        <dd class="order-first">1,520,404</dd>
-                    </span>
-                </div>
+                
             </dl>
         </div>
     </a>
