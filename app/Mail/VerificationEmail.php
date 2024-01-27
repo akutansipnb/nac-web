5e6fc98b3251ab1954b90994c1076a94584b13c9
adeartanugraha <bagusmadearta01@gmail.com>
2024-01-27 13:36:07 +0800

    fixing email verification

--- app/Mail/VerificationEmail.php
+++ app/Mail/VerificationEmail.php
@@ -9,6 +9,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
 
 class VerificationEmail extends Mailable
 {
@@ -31,12 +32,18 @@ class VerificationEmail extends Mailable
      */
     public function build()
     {
+
+        // Ambil data dari public json
+        $path = Storage::disk('local')->get('public/landing_page_config.json');
+        $dataJson = json_decode($path,true);
+
+
         $e = UserDetail::where('id',$this->data['id'])->first();
         $event = Event::where('id',$e->id_events)->first();
         // $users = User::where('id',$e->id_users)->first();
         return $this->from('nac@pnb.ac.id', 'Panitia National Account Competition PNB')->markdown('email.verification-email')->with([
             'group_url' => $event['group_url'],
-            'url_twibon' => $event['url_twibon'],
+            'url_twibon' => $dataJson['url_twibon'],
             'name' => $e->users['name'],
             'email' => $e->users['email'],
             'event' => $event['event_name'],
--- resources/views/email/verification-email.blade.php
+++ resources/views/email/verification-email.blade.php
@@ -1,15 +1,17 @@
 @component('mail::message')
 # Hello , <strong>{{$name}}</strong>
 
-Your registration has been successfully verified, please join  the participant group via the following link
+Your registration has been successfully verified, please join the participant group via the following link
 
 @component('mail::button', ['url' => $group_url])
 Join the Competition Group
 @endcomponent
 
 @component('mail::button', ['url' => $url_twibon])
-Link Twibon
+Download Twibon
 @endcomponent
+
+
 
 Best Regards,<br>
 Committee of National Accounting Competition
